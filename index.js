047ba5529e1e53dadd99420ecebdbd299d649220
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-03-27 21:17:33 -0300

    commit

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const arr = require('./arrays')
 var objeto = arr.objeto
 var matriz = arr.matrizVazia.obj
 
+const notacao = validacao.validar()
+
 var array = arr.array(notacao)
 
 //process.exit(1)
 
-console.log(array)
+
+
+setInterval(() => {
+
+    process.stdout.write("\u001b[2J\u001b[0;0H");
+    process.stdout.write('\033c');
+    console.log('\033[2J');
+    console.table(matriz)
+}, 2000);
+
+    
---
+++ validar.js
@@ -0,0 +1,36 @@
+
+var read = require('readline-sync');
+
+
+function validar() {
+
+    var repete = true
+
+    while(repete) {
+
+      repete = false
+      
+      var notacao = read.question("\ninforme a notação: ", {
+          hideEchoBack: false 
+      });
+
+      notacao = notacao.toLowerCase()
+
+      if(notacao.length !== 2) {
+        console.log("\nA notação deve ter 2 caracteres!")
+        repete = true
+      }
+
+      var i1 = notacao.charCodeAt(0)
+      var i2 = notacao.charCodeAt(1)
+
+      if((i1 < 97 || i1 > 104 || i2 < 49 || i2 > 56) && !repete) {
+        console.log("\nA notação está incorreta!\nO primeiro caracter deve ser uma letra de 'a' a 'h'.\nO segundo caracter eh uma valor de 0 a 8")
+        repete = true
+      }
+    }
+
+  return notacao
+}
+
+module.exports.validar = validar
